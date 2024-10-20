d76f26e56db4749dbd8c94fbb129c25f71bec207
Max Attwell <max.attwell@hotmail.com>
2024-10-20 19:55:46 +1300

    fix: bg locations

--- bin/set-theme.sh
+++ bin/set-theme.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ "$1" == "light" ]; then
     cp ~/.config/waybar/light.css ~/.config/waybar/style.css
-    cp ~/.config/sway/keys-l.png ~/.config/sway/current-bg.png
+    cp ~/dotfiles/backgrounds/keys-l.png ~/.config/sway/current-bg.png
 elif [ "$1" == "dark" ]; then
     cp ~/.config/waybar/dark.css ~/.config/waybar/style.css
-    cp ~/.config/sway/keys-d.png ~/.config/sway/current-bg.png
+    cp ~/dotfiles/backgrounds/keys-d.png ~/.config/sway/current-bg.png
 fi
 # Refresh Waybar and Hyprpaper to apply the new configuration
 killall -SIGUSR2 waybar
