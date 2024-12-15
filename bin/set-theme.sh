4590d4f3d28148f6ed7cc83b04b53e706603d37c
Max Attwell <max.attwell@hotmail.com>
2024-12-15 15:38:32 +1300

    set theme update

--- bin/set-theme.sh
+++ bin/set-theme.sh
@@ -2,14 +2,14 @@
 if [ "$1" == "light" ]; then
     emacsclient -e "(load-theme 'doom-gruvbox-light' t)"
     gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
-    hyprctl hyprpaper wallpaper "DP-1,~/dotfiles/backgrounds/keys-l.png"
+    hyprctl hyprpaper wallpaper "DP-3,~/dotfiles/backgrounds/keys-l.png"
     hyprctl hyprpaper wallpaper "DP-2,~/dotfiles/backgrounds/keys-l.png"
     cp ~/.config/waybar/light.css ~/.config/waybar/style.css
     cp ~/dotfiles/backgrounds/keys-l.png ~/.config/sway/current-bg.png
 elif [ "$1" == "dark" ]; then
     emacsclient -e "(load-theme 'doom-gruvbox' t)"
     gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-    hyprctl hyprpaper wallpaper "DP-1,~/dotfiles/backgrounds/keys-d.png"
+    hyprctl hyprpaper wallpaper "DP-3,~/dotfiles/backgrounds/keys-d.png"
     hyprctl hyprpaper wallpaper "DP-2,~/dotfiles/backgrounds/keys-d.png"
     cp ~/.config/waybar/dark.css ~/.config/waybar/style.css
     cp ~/dotfiles/backgrounds/keys-d.png ~/.config/sway/current-bg.png
